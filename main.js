ff962f7a674f6ed9c95fd60577802c777722535b
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-02-18 16:06:45 +0000

    add html structure in js

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 class Character {
     constructor () {
-        this.characterName
-        this.characterfunction
+        this.name
+        this.utility
     }
 }
 
+class Village {
+    constructor () {
+        this.population
+    }
+
+    display () {
+        
+    }
+
+}
+
+function htmlPart (name, utility) {
+    const characterClass = document.querySelector('#characters-class')
+
+    const characterArticle = document.createElement('article');
+    characterArticle.className = 'character-article';
+    characterArticle.innerHTML = `
+                            <p> ${name} </p>
+                            <p> ${utility} </p>
+                            <button style="margin-bottom:10px" type="button">Remove</button>
+                            <div style="background-color:black; height:0.5px; margin-bottom:10px"></div>
+                            `
+    characterClass.appendChild(characterArticle);
+}
