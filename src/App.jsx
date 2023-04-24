e231fc663679c97631f858b60277acdce0f04452
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-24 09:02:52 +0000

    Add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import "./styles.css"
+
 export default function App () {
-  return "Hi"
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item"/>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+    </>
+  )
 }
