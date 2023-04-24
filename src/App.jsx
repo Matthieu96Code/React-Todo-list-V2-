acbb33c86cec9eb21876f33a5948b00215aaa879
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-24 14:55:27 +0000

    implement an answer if list is empty

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,12 @@ export default function App () {
     })
   }
 
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id )
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -46,6 +52,7 @@ export default function App () {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return (
           <li key={todo.id}>
@@ -55,7 +62,7 @@ export default function App () {
               />
               {todo.title}
             </label>
-            <button className="btn btn-danger">Delete</button>
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
         </li>)
         })}
       </ul>
