1c0a79b389c2ffa18ff7857f2798c0be0d7feb86
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-24 14:41:24 +0000

    Implement toggle checked function

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,18 @@ export default function App () {
     setNewItem("")
   }
 
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -38,7 +50,9 @@ export default function App () {
           return (
           <li key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.completed}/>
+              <input type="checkbox" checked={todo.completed}
+                onChange={e => toggleTodo(todo.id, e.target.checked)}
+              />
               {todo.title}
             </label>
             <button className="btn btn-danger">Delete</button>
