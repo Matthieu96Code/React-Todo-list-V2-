c4480d93c18ccbc1bb6b7a3b79557ffb779cc01e
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-24 12:30:18 +0000

    Implement onchange event from the input

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App () {
+  const [newItem, setNewItem] = useState("")
+
   return (
     <>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item"/>
+          <input 
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            type="text" 
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   )
 }
