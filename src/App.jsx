4b9926c177bd053486abef13ec477c051166d2d3
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-25 10:08:23 +0000

    Set in local storage with use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import NewTodoForm from "./NewTodoForm"
 import TodoList from "./TodoList"
 import "./styles.css"
 
 export default function App () {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null ) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function  addTodo(title) {
     setTodos((currentTodos) => {
